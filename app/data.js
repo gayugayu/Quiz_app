c686e645cce56273309ed03d689a206c123ccead
gayugayu <gayatri1995.11@gmail.com>
2023-06-07 19:11:54 +0530

    quiz-app

---
+++ app/data.js
@@ -0,0 +1,36 @@
+export const quiz ={
+    totalQustion: 5,
+    questions:[
+        {
+            id:1,
+            question:'What is the capital of France ?',
+            answer:['Madrid','Paris','Rome','Berlin'],
+            correctAnswer:'Paris',
+        },
+
+        {
+            id:2,
+            question:'What is the largest planetin our solar system ?',
+            answer:['Mars','Jupiter','Venus','Saturn'],
+            correctAnswer:'Saturn',
+        },
+        {
+            id:3,
+            question:'What is the smallest country in the world ?',
+            answer:['Monaco','Maldives','Vatican city','San Marino'],
+            correctAnswer:'Vatican city'
+        },
+        {
+            id:4,
+            question:'What is the most widely spoken language in the world ?',
+            answer:['English','Mandarin','Spanish','Hindi'],
+            correctAnswer:'Mandarin',
+        },
+        {
+            id:5,
+            question:'Who is the founder of Microsoft ?',
+            answer:['Steve Jobs','Bill Gates' ,'Elon musk','Mark Zukerburg'],
+            correctAnswer:'Bill Gates',
+        }
+    ],
+}
